289fd6b22d86120ecebaecacd5b5e1fffa0288e9
Wyattog <wogden@student.neumont.edu>
2020-02-19 18:27:26 -0700

    added appliance schema

--- mongoose-dal/models/appliances.js
+++ mongoose-dal/models/appliances.js
@@ -1,38 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const books_schema = new Schema({
-        title:{
+const applianceSchema = new Schema({
+        name:{
             type: String,
             required: true,
             index: true,
             unique: true
         },
-        author_first: {
+        desc: {
             type: String,
             required: true,
             index: true,
             unique: true
 
         },
-        author_last: {
+        price: {
             type: String,
             required: true,
             index: true,
             unique: true
         },
-        published: {
-            type: Number,
-            required: true,
-            index: true,
-            unique: true
-        },
-        Illustrator: {
+        
+        time: {
             type: String,
             required: true,
             
         }
 });
 
-const Books = mongoose.model('books', books_schema);
+const Appliances = mongoose.model('Appliances', applianceSchema);
 module.exports = Books;
